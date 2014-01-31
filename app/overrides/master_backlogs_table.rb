9ac11a2e97e22372242759455966b6ea8174ed65
Jacek Grzybowski <jgrzybowski@artegence.com>
2014-01-31 13:34:32 +0100

    deface configured with plugin

---
+++ app/overrides/master_backlogs_table.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => "rb_master_backlogs/show",
+                     :name => "master_backlogs_table",
+                     :replace => "h1",
+                     :text => "<h1>New Post</h1>")
+
+# "../../plugins/redmine_backlogs/app/views/rb_stories/_story.html.erb"
--- init.rb
+++ init.rb
@@ -1,5 +1,12 @@
 require 'redmine'
 
+# Little hack for deface in redmine:
+# - redmine plugins are not railties nor engines, so deface overrides are not detected automatically
+# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
+# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
+Rails.application.paths["app/overrides"] ||= []
+Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
+
 Redmine::Plugin.register :redmine_backlogs_extended_sprints do
   name 'Redmine Backlogs Extended Sprints plugin'
   author 'Jacek Grzybowski'
